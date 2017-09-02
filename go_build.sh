30e338862c9b032f77b63f71737ffbe30f7c5e84
Santosh Heigrujam <tomba.hei@gmail.com>
2017-09-03 01:12:37 +0530

    Remove use of dep

--- go_build.sh
+++ go_build.sh
@@ -10,9 +10,6 @@ go_project_home=$GOPATH/src/github.com/mubitosh/qrshare
 
 cd $GOPATH && mkdir -p src pkg bin $go_project_home
 
-go get -u github.com/golang/dep/cmd/dep
-export PATH=$PATH:$GOPATH/bin
-
 mv vendor $go_project_home/
 mv *.go *.c *.h $go_project_home/
 
@@ -20,6 +17,5 @@ which go
 go version
 
 cd $go_project_home
-dep ensure
 go build -i -o com.github.mubitosh.qrshare -tags gtk_3_18
 mv com.github.mubitosh.qrshare $GOPATH/bin/com.github.mubitosh.qrshare
