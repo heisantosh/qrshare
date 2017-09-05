0eadd949a81b0a248deeb4a399cc7853c9ebec18
Santosh Heigrujam <tomba.hei@gmail.com>
2017-09-05 07:30:33 +0530

    Put dep in PATH and use it

--- go_build.sh
+++ go_build.sh
@@ -7,6 +7,8 @@ bash compile-gresource.sh
 
 # Set up GOPATH and move all the necessary files inside the GOPATH
 export GOPATH="${MESON_SOURCE_ROOT}"
+export PATH=$PATH:$GOPATH/bin
+
 go_project_home=$GOPATH/src/github.com/mubitosh/qrshare
 
 cd $GOPATH && mkdir -p src pkg bin $go_project_home
@@ -16,13 +18,15 @@ mv *.go *.c *.h $go_project_home/
 # So that we can run dep ensure to update the project dependencies
 mv Gopkg.* $go_project_home/
 
-go version
+go env
 
 cd $go_project_home
+
 go get -u github.com/golang/dep/cmd/dep
 echo "dep is installed at" $(which dep)
-ll $GOPATH/bin
-which dep
-if [ $? == 1 ]; do $GOPATH/bin/dep ensure; fi
+ls -l $GOPATH/bin
+
+$(which dep) ensure || $GOPATH/bin/dep ensure || echo "dep command not in PATH" 
+
 go build -ldflags="-s -w" -i -o com.github.mubitosh.qrshare -tags gtk_3_18
 mv com.github.mubitosh.qrshare $GOPATH/bin/com.github.mubitosh.qrshare
