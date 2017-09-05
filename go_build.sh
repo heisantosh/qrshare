3cbf6a0bd40155b11ffe78df219968c7e6f12b8b
Santosh Heigrujam <tomba.hei@gmail.com>
2017-09-05 14:38:25 +0530

    Build the go binary locally instead of building in houston

--- go_build.sh
+++ go_build.sh
@@ -6,26 +6,21 @@ python gen-gresource-go.py
 bash compile-gresource.sh
 
 # Set up GOPATH and move all the necessary files inside the GOPATH
-export GOPATH="${MESON_SOURCE_ROOT}"
+# export GOPATH="${MESON_SOURCE_ROOT}"
+# go_project_home=$GOPATH/src/github.com/mubitosh/qrshare
+# cd $GOPATH && mkdir -p src pkg bin $go_project_home
 
-go_project_home=$GOPATH/src/github.com/mubitosh/qrshare
-
-cd $GOPATH && mkdir -p src pkg bin $go_project_home
-
-mv vendor $go_project_home/
-mv *.go *.c *.h $go_project_home/
+# mv vendor $go_project_home/
+# mv *.go *.c *.h $go_project_home/
 # So that we can run dep ensure to update the project dependencies
-mv Gopkg.* $go_project_home/
+# mv Gopkg.* $go_project_home/
 
-go env
+# go env
 
-cd $go_project_home
-
+# cd $go_project_home
 # go get -u github.com/golang/dep/cmd/dep
+# ls -l $GOPATH/bin
 
-ls -l $GOPATH/bin
-
-$GOPATH/bin/dep ensure
-
-go build -ldflags="-s -w" -i -o com.github.mubitosh.qrshare -tags gtk_3_18
-mv com.github.mubitosh.qrshare $GOPATH/bin/com.github.mubitosh.qrshare
+# $GOPATH/bin/dep ensure
+# go build -ldflags="-s -w" -i -o com.github.mubitosh.qrshare -tags gtk_3_18
+# mv com.github.mubitosh.qrshare $GOPATH/bin/com.github.mubitosh.qrshare
