2ffb9d40cfdcd1e7fef0d78c9214fceeb79e4e22
Santosh Heigrujam <tomba.hei@gmail.com>
2017-09-10 19:16:03 +0530

    Not using dep

--- go_build.sh
+++ go_build.sh
@@ -7,19 +7,12 @@ export GOPATH="${MESON_SOURCE_ROOT}"
 go_project_home=$GOPATH/src/github.com/mubitosh/qrshare
 cd $GOPATH && mkdir -p src pkg bin $go_project_home
 
-cd $GOPATH && wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
-tar -C . -xzf go1.9.linux-amd64.tar.gz
-export PATH=$GOPATH/go/bin:$GOPATH/bin:$PATH
-
 go version
 go env
 
-go install github.com/golang/dep/cmd/dep
-
 mv vendor $go_project_home/
 mv *.go $go_project_home/
-mv Gopkg.* $go_project_home/
 
-cd $go_project_home && dep ensure
+cd $go_project_home
 go build -ldflags="-s -w" -i -o com.github.mubitosh.qrshare -tags $GTK_BUILD_TAG
 mv com.github.mubitosh.qrshare $GOPATH/bin/com.github.mubitosh.qrshare
