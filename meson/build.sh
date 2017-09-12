321161912ab080ac39a9df43a991ee5dbb4ac227
Santosh Heigrujam <tomba.hei@gmail.com>
2017-09-13 00:24:59 +0530

    Update build logic
    During build the project directory is renamed to directory-release-number.
    This confuses gb. Creating a directory as the same name as the project
    and copying the go project files there should do the trick.

--- meson/build.sh
+++ meson/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+mkdir qrshare
+cp -R vendor qrshare/
+cp -R src qrshare/
+cd qrshare
 gb build -tags gtk_3_18 all
-mv bin/qrshare-gtk_3_18 bin/com.github.mubitosh.qrshare
+mv bin/qrshare-gtk_3_18 ../bin/com.github.mubitosh.qrshare
