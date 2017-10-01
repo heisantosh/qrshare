b86edc2de242469d13c06435696fb21a7a920e7e
Santosh Heigrujam <tomba.hei@gmail.com>
2017-10-01 14:50:51 +0530

    Add steps to generate mo files and copy to correct location

--- meson/build.sh
+++ meson/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+appid=com.github.mubitosh.qrshare
+
+# Build the app
 gb build -tags gtk_3_18 all
-mv bin/qrshare-gtk_3_18 bin/com.github.mubitosh.qrshare
+mv bin/qrshare-gtk_3_18 bin/${appid}
+
+# Generate translations for the app
+for po in $(ls po/*.po)
+do
+    name=$(basename $po)
+    lang=${name%.*}
+    mo=po/locale/${lang}/LC_MESSAGES/${appid}.mo
+    mkdir -p po/locale/${lang}/LC_MESSAGES/
+    msgfmt -c -v -o $mo $po
+done
