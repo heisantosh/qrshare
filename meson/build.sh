7584a3037010f41646c42517702c2dcca24496dc
Santosh Heigrujam <tomba.hei@gmail.com>
2018-01-09 23:58:20 +0530

    Use pkg-config to find out GTK+ version instead of hardcoded value

--- meson/build.sh
+++ meson/build.sh
@@ -2,9 +2,12 @@
 
 appid=com.github.mubitosh.qrshare
 
+# GTK+ version
+version=$(pkg-config --modversion gtk+-3.0 | cut -d '.' -f 1,2 | tr . _)
+
 # Build the app
-gb build -tags gtk_3_18 all
-mv bin/qrshare-gtk_3_18 bin/${appid}
+gb build -tags gtk_${version} all
+mv bin/qrshare-gtk_${version} bin/${appid}
 
 # Generate translations for the app
 for po in $(ls po/*.po)
